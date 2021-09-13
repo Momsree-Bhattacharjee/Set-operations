4399db72a2937ad5e6b7205a8b7c806c3b7be6df
Momsree-Bhattacharjee <90470723+Momsree-Bhattacharjee@users.noreply.github.com>
2021-09-13 15:50:48 +0530

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+E = {0 , 2, 4 , 6 , 8}
+N = {1 , 2 , 3 , 4 , 5}
+print ("The uniion of E and N is :", E|N);
+print ("The intersection of E and N is :", E&N);
+print ("The difference of E and N is :", E-N);
+print("The symmetric difference of E and N is :", E^N)
+
+
+# In[ ]:
+
+
+
+
